945c414d95bb6b3430b139e8e568bdf09d0c09ff
wilberdp <wilberdp@users.noreply.github.com>
2023-03-31 16:20:30 -0400

    1

--- pin.js
+++ pin.js
@@ -28,8 +28,20 @@ let PINTextField = _decorate([e$1('pin-field-2')], function (_initialize, _LitEl
       key: "styles",
       value() {
         return i`
-        .pinInput {
-
+        .form-control {
+          color: var(--ntx-form-theme-color-secondary);
+          background-color: var(
+            --ntx-form-theme-color-input-background,
+            transparent
+          );
+          font-size: var(--ntx-form-theme-text-input-size);
+          font-family: var(--ntx-form-theme-font-family);
+          border: 1px solid var(--ntx-form-theme-color-border);
+          border-radius: var(--ntx-form-theme-border-radius);
+        }
+        .form-control:focus {
+          outline: none;
+          border-color: var(--ntx-form-theme-color-primary);
         }
     `;
       }
