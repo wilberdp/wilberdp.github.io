7a1e7fd952e5e21b80c27a548d4be00bdeeee1b3
wilberdp <wilberdp@users.noreply.github.com>
2023-05-10 11:00:12 -0400

    Update repeater-readonly.js

--- repeater-readonly.js
+++ repeater-readonly.js
@@ -1,3 +1,42 @@
+import { html,LitElement} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
+// define the component
+export class HelloWorld extends LitElement {
+  
+  static properties = {
+    who: {type: String},
+  };
+  
+  // return a promise for contract changes.
+  static getMetaConfig() {
+    return {
+      controlName: 'Hello World',
+      fallbackDisableSubmit: false,
+      version: '1.2',
+      properties: {
+        who: {
+          type: 'string',
+          title: 'Who',
+          description: 'Who to say hello to'
+        }
+      }
+    };
+  }
+  
+  constructor() {
+    super();
+    this.who = 'World';
+  }
+
+  render() {
+    return html`<p>Hello ${this.who}<p/>`;
+  }
+}
+
+// registering the web component
+const elementName = 'hello-world';
+customElements.define(elementName, HelloWorld);
+
+/*
 import { _ as _decorate, s, i, e, y, a as e$1 } from './query-assigned-elements-5558b813.js';
 
 const fire = (element, data) => {
@@ -73,3 +112,4 @@ function showHide(attr1) {
 }
 
 export { RepeaterReadOnly };
+*/
