76d50eb6cabf90b0650a8c41de52f97145cfe4d6
wilberdp <wilberdp@users.noreply.github.com>
2024-03-11 16:59:59 -0400

    Add files via upload

--- populate-repeating-section.js
+++ populate-repeating-section.js
@@ -1,5 +1,5 @@
 import { html, LitElement } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
-import { jquery } from 'https://code.jquery.com/jquery-3.7.1.min.js';
+import 'https://code.jquery.com/jquery-3.7.1.min.js';
 
 // define the component
 export class PopulateRepeatingSection extends LitElement {
