67fbec441fadd06a9e2a54ea3aaf553c429d3bcc
wilberdp <wilberdp@users.noreply.github.com>
2024-03-12 17:56:31 -0400

    Add files via upload

--- populate-repeating-section.js
+++ populate-repeating-section.js
@@ -1,6 +1,5 @@
 import { html, LitElement } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 import 'https://cdn.jsdelivr.net/npm/flatpickr';
-import { Component, NgZone, ChangeDetectorRef } from '@angular/core';
 
 // define the component
 export class PopulateRepeatingSection extends LitElement {
