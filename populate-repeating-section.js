98f00147c3a525f49de916c985a05445b876cdca
wilberdp <wilberdp@users.noreply.github.com>
2024-03-12 12:36:47 -0400

    Add files via upload

--- populate-repeating-section.js
+++ populate-repeating-section.js
@@ -45,24 +45,9 @@ export class PopulateRepeatingSection extends LitElement {
             var repeatingSection = document.getElementsByClassName(this.repeatingSectionClass);
             if (repeatingSection != null && repeatingSection.length > 0) {
                 repeatingSection = repeatingSection[0];
+
                 var parsed = JSON.parse(this.values);
-                for (var i = 0; i < parsed.length; i++) {
-                    var idx2 = 0;
-                    var lastSection = repeatingSection.querySelector('.ntx-repeating-section-repeated-section:last-child');
-                    var fields =  lastSection.querySelectorAll('.' + this.repeatingSectionClass + ' input, .' + this.repeatingSectionClass + ' textarea');
-                    for (var key in parsed[i]) {
-                        if (parsed[i].hasOwnProperty(key)) {
-                            console.log(key + ': ' + parsed[i][key]);
-                            if (fields[idx2].classList.contains('flatpickr-input')) {
-                                await new Promise(r => setTimeout(r, 100));
-                                flatpickr(fields[idx2], { allowInput: true, dateFormat: "M d, Y" }).setDate(new Date(parsed[i][key]), true);
-                            }
-                            else {
-                                fields[idx2].value = parsed[i][key];
-                            }
-                            idx2++;
-                        }
-                    }
+                for (var i = 0; i < parsed.length - 1; i++) {
                     var sectionCount = repeatingSection.querySelectorAll('.ntx-repeating-section-repeated-section').length;
                     repeatingSection.parentElement.closest('div').querySelector('button.btn-repeating-section-new-row').click();
                     var exists = false;
@@ -73,6 +58,26 @@ export class PopulateRepeatingSection extends LitElement {
                             exists = true;
                         }
                     }
+                }
+
+                for (var i = 0; i < parsed.length; i++) {
+                    var idx2 = 0;
+                    var lastSection = repeatingSection.querySelector('.ntx-repeating-section-repeated-section:nth-child(' (i + 1) + ')');
+                    var fields =  lastSection.querySelectorAll('.' + this.repeatingSectionClass + ' input, .' + this.repeatingSectionClass + ' textarea');
+                    for (var key in parsed[i]) {
+                        if (parsed[i].hasOwnProperty(key)) {
+                            console.log(key + ': ' + parsed[i][key]);
+                            if (fields[idx2].classList.contains('flatpickr-input')) {
+                                await new Promise(r => setTimeout(r, 500));
+                                flatpickr(fields[idx2], { allowInput: true, dateFormat: "M d, Y" }).setDate(new Date(parsed[i][key]), true);
+                            }
+                            else {
+                                fields[idx2].value = parsed[i][key];
+                            }
+                            idx2++;
+                        }
+                    }
+
                 }
             }
         }
