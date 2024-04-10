7d2d7c033cbe68d6b0c32932d6d9074d33d9f628
wilberdp <wilberdp@users.noreply.github.com>
2024-04-10 16:17:19 -0400

    Add files via upload

--- form-submissions.js
+++ form-submissions.js
@@ -152,31 +152,34 @@ function qs(key) {
 function processContent(data, lang) {
     console.log(data);
     console.log(lang);
+    var dataObj = { "data": data, "lang": lang };
+    console.log(dataObj);
+
     let elements = document.querySelectorAll('p, strong, span, .nx-theme-richtext, .nx-title, .drag-file-label, .nx-upload-button, .nx-action-message');
-    elements.forEach(function (element, idx, data, lang) {
+    elements.forEach(function (element, idx) {
         console.log(idx);
-        element.childNodes.forEach(function (childNode, idx2, data, lang) {
+        element.childNodes.forEach(function (childNode, idx2) {
             if (childNode.nodeType === Node.TEXT_NODE) {
-                data.forEach(function (dataa, idx3, data, lang) {
+                this["data"].forEach(function (dataa, idx3) {
                     if (childNode.textContent.trim().toLowerCase() == dataa.Title.toLowerCase().trim()) {
-                        childNode.textContent = JSON.parse(dataa["Content"])[Object.keys(JSON.parse(dataa["Content"])).find(key => key.toLowerCase() === lang)]
+                        childNode.textContent = JSON.parse(dataa["Content"])[Object.keys(JSON.parse(dataa["Content"])).find(key => key.toLowerCase() === this.toLowerCase())]
                     }
                     if (childNode.textContent.text().trim().toLowerCase() == dataa.Title.toLowerCase().trim() + " *") {
-                        childNode.innerHTML = JSON.parse(dataa["Content"])[Object.keys(JSON.parse(dataa["Content"])).find(key => key.toLowerCase() === lang)] + " <span style='color: #c0504d;'>*</span>";
+                        childNode.innerHTML = JSON.parse(dataa["Content"])[Object.keys(JSON.parse(dataa["Content"])).find(key => key.toLowerCase() === this.toLowerCase())] + " <span style='color: #c0504d;'>*</span>";
                     }
-                }, data, lang);
+                }, this["lang"]);
             }
-        }, data, lang);
-    }, data, lang);
+        }, this);
+    }, dataObj);
         
     elements = document.querySelectorAll('.btn');
-    elements.forEach(function (element, idx, data, lang) {
-        data.forEach(function (dataa, idx2, data, lang) {
+    elements.forEach(function (element, idx) {
+        this["data"].forEach(function (dataa, idx2) {
             if (element.value.trim().toLowerCase() == dataa.Title.toLowerCase().trim()) {
-                element.value = JSON.parse(dataa["Content"])[Object.keys(JSON.parse(dataa["Content"])).find(key => key.toLowerCase() === lang)];
+                element.value = JSON.parse(dataa["Content"])[Object.keys(JSON.parse(dataa["Content"])).find(key => key.toLowerCase() === this.toLowerCase())];
             }
-        }, data, lang);
-    }, data, lang);   
+        }, this["lang"]);
+    }, dataObj);   
 }
 
 
