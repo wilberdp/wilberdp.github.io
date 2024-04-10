e10e5e4f0a4702afcaa978229175b520d46e14c2
wilberdp <wilberdp@users.noreply.github.com>
2024-04-10 16:25:01 -0400

    Add files via upload

--- form-submissions.js
+++ form-submissions.js
@@ -158,18 +158,16 @@ function processContent(data, lang) {
     let elements = document.querySelectorAll('p, strong, span, .nx-theme-richtext, .nx-title, .drag-file-label, .nx-upload-button, .nx-action-message');
     elements.forEach(function (element, idx) {
         console.log(idx);
-        element.childNodes.forEach(function (childNode, idx2) {
-            if (childNode.nodeType === Node.TEXT_NODE) {
-                this["data"].forEach(function (dataa, idx3) {
-                    if (childNode.textContent.trim().toLowerCase() == dataa.Title.toLowerCase().trim()) {
-                        childNode.textContent = JSON.parse(dataa["Content"])[Object.keys(JSON.parse(dataa["Content"])).find(key => key.toLowerCase() === this.toLowerCase())]
-                    }
-                    if (childNode.textContent.text().trim().toLowerCase() == dataa.Title.toLowerCase().trim() + " *") {
-                        childNode.innerHTML = JSON.parse(dataa["Content"])[Object.keys(JSON.parse(dataa["Content"])).find(key => key.toLowerCase() === this.toLowerCase())] + " <span style='color: #c0504d;'>*</span>";
-                    }
-                }, this["lang"]);
-            }
-        }, this);
+        if (element.nodeType === Node.TEXT_NODE) {
+            this["data"].forEach(function (dataa, idx3) {
+                if (element.textContent.trim().toLowerCase() == dataa.Title.toLowerCase().trim()) {
+                    element.textContent = JSON.parse(dataa["Content"])[Object.keys(JSON.parse(dataa["Content"])).find(key => key.toLowerCase() === this.toLowerCase())]
+                }
+                if (element.textContent.text().trim().toLowerCase() == dataa.Title.toLowerCase().trim() + " *") {
+                    element.innerHTML = JSON.parse(dataa["Content"])[Object.keys(JSON.parse(dataa["Content"])).find(key => key.toLowerCase() === this.toLowerCase())] + " <span style='color: #c0504d;'>*</span>";
+                }
+            }, this["lang"]);
+        }
     }, dataObj);
         
     elements = document.querySelectorAll('.btn');
