768bf0e26a58d662a6e7c82afbb8dbcc55cba89b
wilberdp <wilberdp@users.noreply.github.com>
2024-07-10 16:30:16 -0400

    Add files via upload

--- populate-repeating-section-v2.js
+++ populate-repeating-section-v2.js
@@ -346,6 +346,7 @@ function clickPeoplePickerSelection(field, counter) {
 
 function angularize(field) {
     field.dispatchEvent(new Event('input', { bubbles: true }));
+    field.dispatchEvent(new Event('click', { bubbles: true }));
     field.dispatchEvent(new Event('change', { bubbles: true }));
     field.dispatchEvent(new Event('blur', { bubbles: true }));
 }
