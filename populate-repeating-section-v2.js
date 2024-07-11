bce1b0341961adfded2bcccd3bcaee0206b3c2a9
wilberdp <wilberdp@users.noreply.github.com>
2024-07-11 12:33:54 -0400

    Add files via upload

--- populate-repeating-section-v2.js
+++ populate-repeating-section-v2.js
@@ -395,13 +395,10 @@ function angularize(parentElement) {
                 fc2.dispatchEvent(new Event('change', { bubbles: true }));
                 fc2.dispatchEvent(new Event('input', {bubbles: true}));
                 fc2.dispatchEvent(new Event('blur', { bubbles: true }));
-                setTimeout(function (fc2) { 
-                    if (fc2.tagName.toLowerCase() == 'ntx-datetime-picker') {
-                        SelectorAll('input').forEach(function(fc3) {
-                            fc3.dispatchEvent(new CustomEvent('ngModelChange', { bubbles: true }));
-                        });
-                    }
-                }, 1000, fc2);
+
+                if (fc2.closest('ntx-datetime-picker') != null) {
+                    fc2.dispatchEvent(new CustomEvent('ngModelChange', { bubbles: true }));
+                }
             }
         });
     });
