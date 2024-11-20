0cdc324543574e47786db1742d691b8fc4defdc4
wilberdp <wilberdp@users.noreply.github.com>
2024-11-20 10:35:15 -0500

    Add files via upload

--- get-current-user-groups.js
+++ get-current-user-groups.js
@@ -44,32 +44,7 @@ export class GetCurrentUserGroups extends LitElement {
     }
 
     async render2() {
-        try {
-            if (!window || !window.ntxContext || !window.ntxContext.accessTokenProvider) {
-                return new Promise(res => setTimeout(render2, 100));
-            }
-            else {
-                var token = await window.ntxContext.accessTokenProvider.getAccessToken();
-                if (token != null && token != '') {
-                    var url = this.spUrl + '/_api/web/currentuser/?$expand=groups';
-                    const response = await fetch(url, {
-                        headers: {
-                            "Content-Type": "application/json",
-                            "Accept": "application/json;odata=verbose",
-                            "Authorization": "Bearer " + token
-                        }
-                    });
-                    const body = await response.text();
-                    console.log(body);
-                }
-                else {
-                    console.log('token is null or empty');
-                }
-            }
-        }
-        catch(exc) { 
-            console.log(exc);
-        }
+        return new Promise();
     }
 }
 
