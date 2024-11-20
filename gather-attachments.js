744e0b58599a4ab642ad5ba0d8487daf2af6e08b
wilberdp <wilberdp@users.noreply.github.com>
2024-11-20 18:52:25 -0500

    Add files via upload

--- gather-attachments.js
+++ gather-attachments.js
@@ -34,7 +34,7 @@ export class GatherAttachments extends LitElement {
         try {
             setInterval(function(){
                 populateAttachmentJson();
-            }, 100);
+            }, 500);
         }
         catch(exc) { 
             console.log(exc);
