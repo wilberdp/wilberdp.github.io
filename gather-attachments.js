e0bd070b28864d787bf0abdd64046ce1e5502fde
wilberdp <wilberdp@users.noreply.github.com>
2025-02-03 17:49:58 -0500

    Add files via upload

--- gather-attachments.js
+++ gather-attachments.js
@@ -1,7 +1,6 @@
 import { html, LitElement } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 
-var initialAttachments = null;
-var updatedAttachments = null;
+var previousAttachments = null;
 
 // define the component
 export class GatherAttachments extends LitElement {
@@ -47,6 +46,10 @@ function populateAttachmentJson() {
     var json = {};
     json["uploads"] = [];
 
+    if (previousAttachments == null) {
+        previousAttachments = json;
+    }
+
     var fileUploads = document.querySelectorAll('[class*="attachments"]');
     for (var o = 0; o < fileUploads.length; o++) {
         var classes = fileUploads[o].classList;
@@ -72,9 +75,14 @@ function populateAttachmentJson() {
         } 
     }
 
+    var differences = deepDiffMapper.map(previousAttachments, json);
+
+    console.log(differences);
+    return;
+
     if (initialAttachments == null || initialAttachments.uploads == null || initialAttachments.uploads.length == 0) {
-        initialAttachments = {"uploads":[]};
-        document.querySelector('.attachmentsJson textarea').value = JSON.stringify(initialAttachments);
+        initialAttachments = json;
+        document.querySelector('.attachmentsJson textarea').value = JSON.stringify({"uploads":[]});
         document.querySelector('.attachmentsJson textarea').dispatchEvent(new Event('blur'));
     }
     else {       
@@ -145,6 +153,79 @@ function retrieveAttachments(selector) {
     return arr;
 }
 
+var deepDiffMapper = function () {
+    return {
+      VALUE_CREATED: 'created',
+      VALUE_UPDATED: 'updated',
+      VALUE_DELETED: 'deleted',
+      VALUE_UNCHANGED: 'unchanged',
+      map: function(obj1, obj2) {
+        if (this.isFunction(obj1) || this.isFunction(obj2)) {
+            throw 'Invalid argument. Function given, object expected.';
+        }
+        if (this.isValue(obj1) || this.isValue(obj2)) {
+            return {
+            type: this.compareValues(obj1, obj2),
+            data: obj1 === undefined ? obj2 : obj1
+            };
+        }
+
+        var diff = {};
+        for (var key in obj1) {
+            if (this.isFunction(obj1[key])) {
+            continue;
+            }
+
+            var value2 = undefined;
+            if (obj2[key] !== undefined) {
+            value2 = obj2[key];
+            }
+
+            diff[key] = this.map(obj1[key], value2);
+        }
+        for (var key in obj2) {
+            if (this.isFunction(obj2[key]) || diff[key] !== undefined) {
+            continue;
+            }
+
+            diff[key] = this.map(undefined, obj2[key]);
+        }
+
+        return diff;
+
+        },
+        compareValues: function (value1, value2) {
+        if (value1 === value2) {
+            return this.VALUE_UNCHANGED;
+        }
+        if (this.isDate(value1) && this.isDate(value2) && value1.getTime() === value2.getTime()) {
+            return this.VALUE_UNCHANGED;
+        }
+        if (value1 === undefined) {
+            return this.VALUE_CREATED;
+        }
+        if (value2 === undefined) {
+            return this.VALUE_DELETED;
+        }
+        return this.VALUE_UPDATED;
+        },
+        isFunction: function (x) {
+        return Object.prototype.toString.call(x) === '[object Function]';
+        },
+        isArray: function (x) {
+        return Object.prototype.toString.call(x) === '[object Array]';
+        },
+        isDate: function (x) {
+        return Object.prototype.toString.call(x) === '[object Date]';
+        },
+        isObject: function (x) {
+        return Object.prototype.toString.call(x) === '[object Object]';
+        },
+        isValue: function (x) {
+        return !this.isObject(x) && !this.isArray(x);
+        }
+    }
+}();
 
 // registering the web component
 const elementName = 'gather-attachments';
