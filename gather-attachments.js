9c8faa221601fcd302b0327591861ed13e189fb2
wilberdp <wilberdp@users.noreply.github.com>
2025-02-03 18:10:29 -0500

    Add files via upload

--- gather-attachments.js
+++ gather-attachments.js
@@ -76,7 +76,7 @@ function populateAttachmentJson() {
     }
 
     var differences = deepDiffMapper.map(previousAttachments, json);
-    if (differences != null && differences.uploads != null && differences.uploads.length > 0) {
+    if (differences != null && differences.uploads != null && differences.uploads[0] != null) {
         console.log(differences);
         previousAttachments = json;
     }
