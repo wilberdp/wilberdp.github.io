724656256d3191a8a4ac622ac60c6b351a488dff
wilberdp <wilberdp@users.noreply.github.com>
2025-02-04 15:27:32 -0500

    Add files via upload

--- gather-attachments.js
+++ gather-attachments.js
@@ -76,80 +76,28 @@ function populateAttachmentJson() {
     
     var dataToOutput = {"upload":[]};
 
-    console.log(previousAttachments);
-    console.log(json);
+    //console.log(previousAttachments);
+    //console.log(json);
 
-    console.log(JSON.stringify(previousAttachments) === JSON.stringify(json));
+    //console.log(JSON.stringify(previousAttachments) === JSON.stringify(json));
 
-    var additions = getObjectDifferences(previousAttachments, json);
+    var differences = getObjectDifferences(previousAttachments, json);
     console.log('differences');
-    console.log(additions);
+    console.log(differences);
 
     previousAttachments = structuredClone(json);
+
+    if (differences != null && differences["uploads"] != null) {
+        for (var topLevelDifference in differences) {
+            console.log(topLevelDifference);
+        }
+    }
+
     
     //console.log(dataToOutput);
     //document.querySelector('.attachmentsJson textarea').value = JSON.stringify(dataToOutput);
     //document.querySelector('.attachmentsJson textarea').dispatchEvent(new Event('blur'));
     return;
-
-    if (initialAttachments == null || initialAttachments.uploads == null || initialAttachments.uploads.length == 0) {
-        initialAttachments = json;
-        document.querySelector('.attachmentsJson textarea').value = JSON.stringify({"uploads":[]});
-        document.querySelector('.attachmentsJson textarea').dispatchEvent(new Event('blur'));
-    }
-    else {       
-        var jsonKeys = json['uploads'].map(function(itt) { return itt['name']; });
-        var jsonValues = json['uploads'].map(function(itt) { return itt['values']; });
-        var initialKeys = initialAttachments['uploads'].map(function(itt) { return itt['name']; });
-        var initialValues = initialAttachments['uploads'].map(function(itt) { return itt['values']; });
-
-        if (updatedAttachments == null) {
-            updatedAttachments = {"upload":[]};
-        } 
-
-        // remove
-        for (var i = 0; i < initialKeys.length; i++) {
-            if (jsonKeys.indexOf(initialKeys[i]) == -1) {
-                updatedAttachments['uploads'] = updatedAttachments['uploads'].filter(function(itt){ return itt['name'] != initialKeys[i] });
-            }
-            else {
-                for (var o = 0; o < initialValues[i].length; o++) {
-                    var newJsonEntry = updatedAttachments['uploads'].filter(function(itt) { return itt['name'] == initialKeys[i] });
-                    var jsonEntry = json['uploads'].filter(function(itt) { return itt['name'] == initialKeys[i] });
-                    if (newJsonEntry != null && newJsonEntry.length > 0 && jsonEntry != null && jsonEntry.length > 0 && jsonEntry[0] != null && jsonEntry[0]['values'] != null) {
-                        var idx = jsonEntry[0]['values'].indexOf(initialValues[i][o]);
-                        if (idx == -1) {
-                            newJsonEntry[0]['values'].splice(o, 1);
-                        }                    
-                    }
-                }
-            }
-        }
-
-        // add new
-        for (var i = 0; i < jsonKeys.length; i++) {
-            if (initialKeys.indexOf(jsonKeys[i]) == -1) {
-                updatedAttachments["uploads"].push({'name': jsonKeys[i], 'values': []})
-            }
-
-            var newJsonEntry = updatedAttachments['uploads'].filter(function(itt) { return itt['name'] == jsonKeys[i] });         
-            var values = jsonValues[i];
-            if (values != null && values.length > 0) {
-                for (var o = 0; o < values.length; o++) {
-                    if (newJsonEntry != null && newJsonEntry.length > 0 && newJsonEntry[0] != null && newJsonEntry[0]['values'] != null) {
-                        if (newJsonEntry[0]['values'].indexOf(values[o]) == -1) {
-                            newJsonEntry[0]['values'].push(values[o]);
-                        }
-                    }
-                }    
-            }                 
-        }
-        
-        document.querySelector('.attachmentsJson textarea').value = JSON.stringify(updatedAttachments);
-        document.querySelector('.attachmentsJson textarea').dispatchEvent(new Event('blur'));
-
-        //initialAttachments = json;
-    }
 }
 
 function retrieveAttachments(selector) {
