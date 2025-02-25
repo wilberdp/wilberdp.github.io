2cdb98b880ab86055e817294f4da23f1dd384433
wilberdp <wilberdp@users.noreply.github.com>
2025-02-25 11:31:54 -0500

    Add files via upload

--- currently-assigned.js
+++ currently-assigned.js
@@ -61,7 +61,7 @@ export class CurrentlyAssigned extends LitElement {
             else {
                 var token = await window.ntxContext.accessTokenProvider.getAccessToken();
                 if (token != null && token != '') {
-                    var url = this.spUrl + '/_api/web/lists/getbytitle("' + this.taskListTitle + '")/items(' + this.itemId + ')?select=AssigneeID';
+                    var url = this.spUrl + '/_api/web/lists/getbytitle(\'' + this.taskListTitle + '\')/items(' + this.itemId + ')?select=AssigneeID';
                     const response = await fetch(url, {
                         headers: {
                             "Content-Type": "application/json",
