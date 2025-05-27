2968b7cfab4d0eb914bb6d2ab06dcfe73722f01b
wilberdp <wilberdp@users.noreply.github.com>
2025-05-27 16:03:48 -0400

    Add files via upload

---
+++ clear-file-uploads.js
@@ -0,0 +1,80 @@
+import { html, LitElement } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
+
+export class ClearFileUploads extends LitElement {
+    static properties = {
+        hasRun: { type: Boolean },
+        classToTarget: { type: String },
+    };        
+
+    static getMetaConfig() {
+        return {
+            controlName: 'ClearFileUploads',
+            fallbackDisableSubmit: false,
+            version: '1.0',
+            standardProperties: {
+                visibility: true
+            },
+            properties: {
+                classToTarget: {
+                    title: 'Class to Target',
+                    type: 'string'
+                },
+                hasRun: {
+                    title: 'Has Run',
+                    type: 'boolean',
+                    isValueField: true
+                }
+            },
+            events: ['ntx-value-change']
+        };
+    }
+  
+    constructor() {
+        super();
+    }
+
+    render() {
+        if (!this.hasRun) {
+            this.render2().then(res => {
+                console.log(res);            
+            });   
+        }
+
+        return html`<p>ClearFileUploads</p>`;
+    }
+
+    async render2() {
+        try {
+            var theInterval = setInterval(function(){
+                var fileUploads = document.querySelectorAll('.' + this.classToTarget);
+                if (fileUploads != null) {
+                    for (var i = 0; i < fileUploads.length; i++) {
+                        var toTrash = fileUploads[i].querySelectorAll('[data-e2e="trash"]');
+                        if (toTrash != null) {
+                            while (toTrash.length < 0) {
+                                toTrash[0].dispatchEvent(new Event('click'));
+                                toTrash = fileUploads[i].querySelectorAll('[data-e2e="trash"]');
+                            }
+                        }
+                    }
+                    this.hasRun = true;
+                    clearInterval(theInterval);
+                }
+            }, 500);
+        }
+        catch(exc) { 
+            console.log(exc);
+        }
+    }
+}
+
+
+
+
+function deepEqual(a, b) {
+    return JSON.stringify(a) === JSON.stringify(b);
+}
+
+// registering the web component
+const elementName = 'clear-file-uploads';
+customElements.define(elementName, ClearFileUploads);
