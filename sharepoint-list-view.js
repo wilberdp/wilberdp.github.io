5131d2f4eb21423e5cf26622840f62d9e40fcb33
wilberdp <wilberdp@users.noreply.github.com>
2025-07-11 17:45:01 -0400

    Add files via upload

--- sharepoint-list-view.js
+++ sharepoint-list-view.js
@@ -175,7 +175,7 @@ export class SharepointListView extends LitElement {
             for (var o = 0; o < listItemData.length; o++) {
                 htmlView += "<tr>";
                 for (var i = 0; i < fieldRefs.length; i++) { 
-                    htmlView += "<td>" + $this.getFieldValue(fieldRef, listItemData[o]) + "</td>";
+                    htmlView += "<td>" + $this.getFieldValue(fieldRefs[i], listItemData[o]) + "</td>";
                 }
                 htmlView += "</tr>";
             }
@@ -191,7 +191,7 @@ export class SharepointListView extends LitElement {
 
     getFieldValue(fieldRef, item) {
         console.log(fieldRef);
-        
+
         var value = item[fieldRef.attributes["Name"].nodeValue];
 
         return value;
