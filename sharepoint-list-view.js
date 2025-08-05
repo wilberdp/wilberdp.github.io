e71745206e527bd69757d16a8a98dbedf3f273a8
wilberdp <wilberdp@users.noreply.github.com>
2025-08-05 14:46:46 -0400

    Add files via upload

--- sharepoint-list-view.js
+++ sharepoint-list-view.js
@@ -150,7 +150,43 @@ export class SharepointListView extends LitElement {
                 var customJavascript = document.createElement('script');
                 customJavascript.type = 'text/javascript';
                 customJavascript.text = `var thisId = 'sharepoint-list-view-${$this.listViewNumber}';`;
-                customJavascript.text += `var thisShadowDom = ${this.shadowRoot}; console.log(thisShadowDom);`;
+                customJavascript.text += `$$$(selector, rootNode = document.body) {
+                    const arr = []
+                    
+                    const traverser = node => {
+                        // 1. decline all nodes that are not elements
+                        if(node.nodeType !== Node.ELEMENT_NODE) {
+                            return
+                        }
+                        
+                        // 2. add the node to the array, if it matches the selector
+                        if(node.matches(selector)) {
+                            arr.push(node)
+                        }
+                        
+                        // 3. loop through the children
+                        const children = node.children
+                        if (children.length) {
+                            for(const child of children) {
+                                traverser(child)
+                            }
+                        }
+                        
+                        // 4. check for shadow DOM, and loop through it's children
+                        const shadowRoot = node.shadowRoot
+                        if (shadowRoot) {
+                            const shadowChildren = shadowRoot.children
+                            for(const shadowChild of shadowChildren) {
+                                traverser(shadowChild)
+                            }
+                        }
+                    }
+                    
+                    traverser(rootNode)
+                    
+                    return arr
+                }`;
+                customJavascript.text += `var thisShadowDom = $$$(thisId); console.log(thisShadowDom);`;
                 customJavascript.text += this.customJavascript;
                 this.shadowRoot.appendChild(customJavascript);
             }
